75dc71f1cb2bdd19b1c0f5a6d73fa347f3859e9f
tundrabr <joanitamonteiro2023@gmail.com>
2025-11-26 22:46:35 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold">TundraAI</h1>
-      <p>Gere seu mini-negócio com IA.</p>
-      <a href="/dashboard">Ir para Dashboard</a>
-    </main>
-  );
-}
